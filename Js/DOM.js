7d0d0194e5941f5b5639d4e74d1169d2b88e711b
angeluc1343 <angeluc2007@gmail>
2021-09-08 17:15:21 -0500

    js

--- Js/DOM.js
+++ Js/DOM.js
@@ -1,3 +1,8 @@
 const changecolor = (color) =>{
     document.body.style.backgroundColor = color;
 }
+
+const cambiarImagen = () =>{
+    imagen.src ="../src/angel 32.png"
+    titulo.innerHTML = "DOM"
+}
